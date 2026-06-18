1627d6af784bcf7f877c119c4ab4bfabfab9ce1e
nirban11233 <nirbanchk@gmail.com>
2026-06-18 23:49:04 +0000

    Remove Expo mobile projects - switching to Android Studio builds

--- mobile/admin-app/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { DashboardScreen } from "./src/screens/DashboardScreen";
-
-export default function App() {
-  return (<><StatusBar style="dark" /><DashboardScreen /></>);
-}
